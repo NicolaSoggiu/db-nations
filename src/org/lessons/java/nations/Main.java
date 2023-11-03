66348b24922a04147be2b55e0c10ced7cfec8598
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-11-03 15:45:57 +0100

    add bonus (to fix)

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -20,11 +20,12 @@ public class Main {
                             "FROM countries c " +
                             "JOIN regions r ON c.region_id = r.region_id " +
                             "JOIN continents cn ON r.region_id = cn.continent_id " +
-                            "WHERE c.name like ?" +
-                            "ORDER BY c.name ;";
+                            "WHERE c.name LIKE ? " +
+                            "ORDER BY c.name;";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
                 preparedStatement.setString(1, "%" + userCountry + "%");
+
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         String countryName = resultSet.getString("Country_name");
@@ -35,6 +36,46 @@ public class Main {
                                 "\tCountry Id: " + countryId + " " +
                                 "\tRegion Name: " + regionName + " " +
                                 "\tContinent Name: " + continentName);
+
+                        System.out.println("Insert a country id: ");
+                        String countryIdInput = scan.nextLine();
+
+                        String queryLang =
+                                "SELECT l.`language` as Languages " +
+                                        "FROM country_languages cl " +
+                                        "JOIN languages l ON cl.language_id = l.language_id " +
+                                        "WHERE cl.country_id = ?";
+
+                        try (PreparedStatement langPreparedStatement = connection.prepareStatement(queryLang)) {
+                            langPreparedStatement.setString(1, countryIdInput);
+                            ResultSet langResultSet = langPreparedStatement.executeQuery();
+                            if (langResultSet.next()) {
+                                String languages = langResultSet.getString("Languages");
+                                System.out.println("Languages: " + languages);
+                            }
+                        } catch (SQLException e) {
+                            System.out.println("Unable to execute language query");
+                            e.printStackTrace();
+                        }
+
+                        String queryStats =
+                                "SELECT `year`, population, gdp " +
+                                        "FROM country_stats " +
+                                        "WHERE country_id = ?";
+
+                        try (PreparedStatement statsPreparedStatement = connection.prepareStatement(queryStats)) {
+                            statsPreparedStatement.setString(1, countryIdInput);
+                            ResultSet statsResultSet = statsPreparedStatement.executeQuery();
+                            while (statsResultSet.next()) {
+                                String year = statsResultSet.getString("year");
+                                String population = statsResultSet.getString("population");
+                                String gdp = statsResultSet.getString("gdp");
+                                System.out.println("Year: " + year + ", Population: " + population + ", GDP: " + gdp);
+                            }
+                        } catch (SQLException e) {
+                            System.out.println("Unable to execute country stats query");
+                            e.printStackTrace();
+                        }
                     }
                 } catch (SQLException e) {
                     System.out.println("Unable to execute query");
