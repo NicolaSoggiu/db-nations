7066f7de765642500a4452ef1b137b1814deb161
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-11-03 15:03:20 +0100

    add milestone 3

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -1,22 +1,30 @@
 package org.lessons.java.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         String url = "jdbc:mysql://localhost:3306/db_nations";
         String user = "root";
         String password = "root";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Search a country: ");
+            String userCountry = scan.nextLine();
+
             String query =
                     "SELECT c.name as Country_name, c.country_id as Id, r.name as Region_name, cn.name as Continent_name " +
                             "FROM countries c " +
                             "JOIN regions r ON c.region_id = r.region_id " +
                             "JOIN continents cn ON r.region_id = cn.continent_id " +
+                            "WHERE c.name like ?" +
                             "ORDER BY c.name ;";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, "%" + userCountry + "%");
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         String countryName = resultSet.getString("Country_name");
@@ -40,5 +48,7 @@ public class Main {
             System.out.println("Unable to open connection");
             e.printStackTrace();
         }
+
+        scan.close();
     }
 }
